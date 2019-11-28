226615d9433018bdba25bc4fd0f906026f0cf8c6
Copyright2015 <copyright9010@gmail.com>
2019-11-28 11:39:28 +0600

    Добавлены новые миграции для переопределения связей между юзерами и ролями

---
+++ database/migrations/2019_11_28_051302_change_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Переделываем связи между юзерами и ролями.
+        Schema::table('roles', function (Blueprint $table) {
+            //
+            $table->dropForeign('roles_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::create('roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+}
