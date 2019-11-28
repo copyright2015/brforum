6c6b8d98c52e27e50cf1d0c622085cc49eb0e908
Copyright2015 <Copyright9010@gmail.com>
2019-11-28 23:01:36 +0600

    Мониторинг Sql запросов.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        DB::listen(function ($query){
+            dump($query->sql);
+            dump($query->bindings);
+        });
     }
 }
